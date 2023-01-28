045c964bd03798bb4664dde7c9153fe4e64975b2
Resul <resulozdmr6@icloud.com>
2023-01-28 05:20:06 +0300

    Initial Commit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,25 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Resul Özdemir on 28.01.2023.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    
+    init(simpsonName: String, simpsonJob:String,simpsonImage:UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+}
+
